f62b9ccf51fed19831a2fe9616b791b1668ed7e8
kout2 <tajitajifj0133@gmail.com>
2026-07-11 17:20:56 +0900

    Fix local Activity environment loading

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,4 +1,10 @@
-import "dotenv/config";
+﻿import { config as loadDotenv } from "dotenv";
+import { fileURLToPath } from "node:url";
+
+loadDotenv({
+  path: fileURLToPath(new URL("../../../.env", import.meta.url))
+});
+
 import { createApp } from "./app.js";
 import { loadEnv } from "./env.js";
 
@@ -7,4 +13,5 @@ const app = createApp({ env });
 
 app.listen(env.PORT, () => {
   console.log(`iris-casino-activity server listening on ${env.PORT}`);
+  console.log(`allowed web origin: ${env.WEB_ORIGIN}`);
 });
